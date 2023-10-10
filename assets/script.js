bfeb76f67c3847fd86f58f1a20adf6b8fca1010c
austintynes <austintynes10@gmail.com>
2023-10-10 16:00:09 -0500

    Fighter input styling

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// DOM elements
+
+//First & Last name  /    Fight mins & Fight secs
+const firstNameEl = document.querySelector('#firstName');
+const lastNameEl = document.querySelector('#lastName');
+const totalFightsEl = document.querySelector('#total-fights');
+// UFC fights and fight time
+const ufcFightsEl = document.querySelector('#ufc-fights');
+const fightMinsEl = document.querySelector('#fight-mins');
+const fightSecsEl = document.querySelector('#fight-secs');
+// Striking stats
+const strikingAccEl = document.querySelector('#striking-acc');
+const strikingDefEl = document.querySelector('#striking-def');
+const strikesLpmEl = document.querySelector('#strikesLpm');
+const strikesApmEl = document.querySelector('#strikesApm');
+// Striking power
+const kdPer15El = document.querySelector('#kd-per15');
+// Striking distribution
+const strikesStandEl = document.querySelector('#strikes-standing');
+const strikesClinchEl = document.querySelector('#strikes-clinch');
+const strikesGroundEl = document.querySelector('#strikes-ground');
+// Grappling stats
+const tdAcc = document.querySelector('#strikes-standing');
+const tdDef = document.querySelector('#strikes-clinch');
+const tdLpm = document.querySelector('#strikes-ground');
+
+// Win distribution 
+const winDecEl = document.querySelector('#win-dec');
+const winKoEl = document.querySelector('#win-ko');
+const winSubEl = document.querySelector('#win-sub');
+// Loss distribution 
+const lossDecEl = document.querySelector('#loss-dec');
+const lossKoEl = document.querySelector('#loss-ko');
+const lossSubEl = document.querySelector('#loss-sub');
+//
+
+
+
+
+
+
